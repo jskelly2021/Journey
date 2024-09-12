ceb74432bf309427bc7ac20d93e4b8f937b7704c
Jacob Kelly <jskelly2021@gmail.com>
2024-09-12 16:02:23 -0500

    Scenes can be unloaded, loaded, or set active asynchronously

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
 using Eflatun.SceneReference;
 using System;
+using System.Collections.Generic;
 
 public class SceneController : MonoBehaviour
 {
     [SerializeField] private SceneReference sceneToLoad;
     [SerializeField] private SceneReference sceneToUnload;
 
-    public static event Action<SceneReference, bool> LoadScene;
+    public static event Action<SceneReference> LoadScene;
     public static event Action<SceneReference> UnloadScene;
+    public static event Action<SceneReference> SetSceneActive;
 
-    public void loadScene(bool setActive)
+    public void loadScene()
     {
-        LoadScene(sceneToLoad, setActive);
+        LoadScene(sceneToLoad);
     }
 
     public void unloadScene()
     {
         UnloadScene(sceneToUnload);
+    }
+
+    public void setSceneActive()
+    {
+        SetSceneActive(sceneToLoad);
     }
 
     public void quitGame()
