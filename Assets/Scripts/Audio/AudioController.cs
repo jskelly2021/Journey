aaff64690bf461dd70d1f8d54c34a848e0e2e4bd
Jacob Kelly <jskelly2021@gmail.com>
2024-09-22 21:21:49 -0500

    Updated the setVolume funtion to make the volume slider work linearly

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -5,5 +5,5 @@ public class AudioController : MonoBehaviour
     [SerializeField] private AudioClip audioClip;
 
     public void playAudioClip() => AudioEventManager.playAudio(audioClip);
-    public void setMasterVolume(float volumeLevel) => AudioEventManager.setVolume("MasterVolume", volumeLevel);
+    public void setMasterVolume(float volumeLevel) => AudioEventManager.setVolume("MasterVolume", Mathf.Log10(volumeLevel) * 20f);
 }
