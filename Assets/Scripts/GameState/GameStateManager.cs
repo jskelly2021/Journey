378a4a6af90f4d6c1ebe9d29d37a1ed7f79ceb22
Jacob Kelly <jskelly2021@gmail.com>
2024-09-30 16:44:07 -0500

    GameStateManager now only listens for GameStateEvents

--- Assets/Scripts/GameState/GameStateManager.cs
+++ Assets/Scripts/GameState/GameStateManager.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public sealed class GameManager : MonoBehaviour
+public sealed class GameStateManager : MonoBehaviour
 {
-    private static GameManager instance = null;
+    private static GameStateManager instance = null;
     private SceneLoader sceneLoader;
 
-    public GameManager Instance { get { return instance; } }
+    public GameStateManager Instance { get { return instance; } }
 
     private void Awake()
     {
@@ -17,20 +17,11 @@ public sealed class GameManager : MonoBehaviour
 
     private void OnEnable()
     {
-        InputHandler.PauseAction += pauseGame;
-        InputHandler.ResumeAction += resumeGame;
-        QuitController.QuitGame += quit;
+        GameStateEvents.OnSetGameState += handleGameState;
     }
     private void OnDisable()
     {
-        InputHandler.PauseAction -= pauseGame;
-        InputHandler.ResumeAction -= resumeGame;
-        QuitController.QuitGame -= quit;
-    }
-
-    private void pauseGame()
-    {
-        Time.timeScale = 0.0f;
+        GameStateEvents.OnSetGameState -= handleGameState;
     }
 
     private void resumeGame()
@@ -38,8 +29,32 @@ public sealed class GameManager : MonoBehaviour
         Time.timeScale = 1.0f;
     }
 
-    private void quit()
+    private void pauseGame()
     {
+        Time.timeScale = 0.0f;
+    }
+
+    private void quitGame()
+    {
+        Debug.Log("Quitting Game");
         Application.Quit();
+    }
+
+    private void handleGameState(GameStates gameState)
+    {
+        switch (gameState)
+        {
+            case GameStates.Play:
+                resumeGame();
+                break;
+            case GameStates.Pause:
+                pauseGame();
+                break;
+            case GameStates.Quit:
+                quitGame();
+                break;
+            default:
+                break;
+        }
     }
 }
